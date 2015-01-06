92e8f6bba07ef39a6d22cde373c778509e9b41e3
rjmccluskey <rjmccluskey@gmail.com>
2015-01-05 23:04:19 -0800

    adds basic text chatting

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,2 +1,13 @@
+var socket = io()
+
 $(document).ready(function(){
+  $('#chat_form').on('submit', function(){
+    socket.emit('chat message', $('#chat_input').val())
+    $('#chat_input').val("")
+    return false
+  })
+
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg))
+  })
 })
