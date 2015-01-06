47f7bb77e07f8d3429740c8768cf3e050bfe1695
rjmccluskey <rjmccluskey@gmail.com>
2015-01-06 09:30:21 -0800

    adds notifications for number of users

--- sockets/base.js
+++ sockets/base.js
@@ -1,12 +1,21 @@
 module.exports = function(io) {
+  var userCount = 0
 
   io.on('connection', function(socket) {
+    console.log('a user connected')
+    userCount++
+    console.log(userCount + ' user(s) connected')
+    io.emit('chat message', '** a user connected ** (total users: ' + userCount + ')')
 
     socket.on('chat message', function(msg){
       io.emit('chat message', msg)
     })
 
     socket.on('disconnect', function(){
+      console.log('a user disconnected')
+      userCount--
+      console.log(userCount + ' user(s) connected')
+      io.emit('chat message', '** a user disconnected ** (total users: ' + userCount + ')')
     })
   })
 }
