be73d5f527b6e9ca53e2867215e769b018ba285e
rjmccluskey <rjmccluskey@gmail.com>
2015-01-07 12:45:02 -0800

    adds giphy gifs to the chat message, needs better style

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,35 +6,34 @@ $(document).ready(function(){
   socket.on('chat message', appendMessage)
 })
 
-
-
-
-
-function emitMessage(event) {
-  // emits the message to all users
-  if ($('#get_gif_window').length === 0) {
-    getGIFSearchForm()
-
-    // socket.emit('chat message', $('#chat_input').val())
-    // clearInput()
-  }
-  return false
-}
-
-function clearInput() {
-  // clears the text input box after user sends message
-  $('#chat_input').val('')
+function addEventListeners() {
+  $('#chat_form').on('submit', promptForGIF)
+  $('body').on('submit','#search_giphy_form', getGiphyGIFs)
+  $('body').on('click','.giphy_result', emitMessage)
 }
 
 function appendMessage(msg) {
   // appends message to chat window
-  $('#messages').append($('<li>').text(msg))
+  $('#messages').append(msg)
 }
 
-function addEventListeners() {
-  $('#chat_form').on('submit', emitMessage)
-  $('body').on('submit','#search_giphy_form', getGiphyGIFs)
+
+
+function promptForGIF(event) {
+  // emits the message to all users
+  if ($('#get_gif_window').length === 0) {
+    getGIFSearchForm()
+  }
+  return false
 }
+
+function resetChatInput() {
+  // clears the GIF form and text input box
+  $('#get_gif_window').remove()
+  $('#chat_input').val('')
+}
+
+
 
 
 function getGIFSearchForm() {
@@ -52,7 +51,16 @@ function getGiphyGIFs(event) {
     type: 'GET',
     url: '/gifs/search?giphysearch=' + $('#search_giphy_input').val(),
   }).done(function(data){
-    console.log(data)
     $('#get_gif_window').append(data)
   })
+}
+
+function emitMessage() {
+  var url = $(this).attr('src')
+  var message = '<li><img src="' + url + '" class="gif"><p>' + $('#chat_input').val() + '</p></li>'
+  console.log(url)
+  console.log(message)
+
+  socket.emit('chat message', message)
+  resetChatInput()
 }
