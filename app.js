b158103137ddb4405a12605932cf67916f4ac3e9
rjmccluskey <rjmccluskey@gmail.com>
2015-01-08 14:36:04 -0800

    adds working sessions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -23,6 +24,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,21 @@ var router = express.Router();
 //   res.send('respond with a resource');
 // });
 
+router.get('/', function(req,res) {
+  var user
+  if (req.session.user_id) {
+    user = models.User.find({
+      where: { id: req.session.user_id }
+    }).then(function(user) {
+      res.send('Hello ' + user.username + '!')
+      user.destroy()
+    })
+  }
+  else {
+    res.send('No user signed in!')
+  }
+})
+
 /* Create new user after signing up and redirect to chat room */
 router.post('/signup', function(req, res) {
   var username = req.param('username')
@@ -19,7 +34,9 @@ router.post('/signup', function(req, res) {
     models.User.find({
       where: { username: username }
     }).then(function() {
-      console.log(user.id)
+      req.session.user_id = user.id
+    }).then(function() {
+      res.redirect('/users')
     })
   })
 })
