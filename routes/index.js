bca466380c58f2895b4493391533cf0aad214833
rjmccluskey <rjmccluskey@gmail.com>
2015-01-08 12:35:29 -0800

    adds beginnings to user sign up route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-var models = require('../models')
 var express = require('express');
 var router = express.Router();
 var rest = require('restler')
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res) {
+//   res.send('respond with a resource');
+// });
+
+/* Create new user after signing up and redirect to chat room */
+router.post('/signup', function(req, res) {
+  var username = req.param('username')
+  var password = req.param('password1')
+
+  models.User.create({
+    username: username,
+    password: password
+  }).then(function(user) {
+    models.User.find({
+      where: { username: username }
+    }).then(function() {
+      console.log(user.id)
+    })
+  })
+})
 
 module.exports = router;
